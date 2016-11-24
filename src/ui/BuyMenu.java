1e74277b384f2b8ffeb22255976b592e55c20d01
OOP <opkampitak@hotmail.com>
2016-11-24 21:15:21 +0700

    Forgot this file

--- src/ui/BuyMenu.java
+++ src/ui/BuyMenu.java
@@ -3,11 +3,11 @@ package ui;
 import javafx.scene.control.ListView;
 import model.RenderableHolder;
 
-public class BuyMenu extends ListView<BuyItem>{	
+public class BuyMenu extends ListView<BuyItem>{
 	public BuyMenu() {
 		// TODO Auto-generated constructor stub
-		getItems().add(new BuyItem(RenderableHolder.tileObject_stone_img,"Stone"));
-		getItems().add(new BuyItem(RenderableHolder.tileObject_tree_img,"Tree"));
+		getItems().add(new BuyItem("Tree",RenderableHolder.tileObject_stone_img));
+		getItems().add(new BuyItem("Stone",RenderableHolder.tileObject_tree_img));
 	}
 
 }
