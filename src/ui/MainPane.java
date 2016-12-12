69ac217c1ceaeb014786c569efa3f9b0eb83810f
PalmPTSJ <palm_bubui@hotmail.com>
2016-12-12 22:56:03 +0700

    New main menu

--- src/ui/MainPane.java
+++ src/ui/MainPane.java
@@ -5,11 +5,9 @@ import javafx.application.Platform;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundImage;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import logic.SoundManager;
 import thread.showHighscoreThread;
@@ -17,24 +15,16 @@ import thread.showHighscoreThread;
 public class MainPane extends VBox implements IStoppable {
 	Thread joiner;
 	public MainPane() {
-		
-		HBox title=new HBox();
-		Label titleName=new Label("Super Killing War");
-		Label titleVersion=new Label("1.0.1");
 		Button start=new Button("Start");
 		Button exit=new Button("Exit");
 		Button highscore = new Button("High score");
 		VolumePane volume=new VolumePane();
-		getChildren().addAll(title,start,exit,highscore,volume);
-		setAlignment(Pos.CENTER);
+		getChildren().addAll(start,exit,highscore,volume);
+		
+		setAlignment(Pos.BOTTOM_RIGHT);
 		setPrefSize(Main.screenWidth+300, Main.screenHeight);
 		
 		setBackground(new Background(new BackgroundImage(new Image(ClassLoader.getSystemResource("img/ui/background.png").toString()), null, null, null,null)));
-		title.setAlignment(Pos.TOP_CENTER);
-		titleName.getStyleClass().setAll("h1","text-primary");
-		titleVersion.getStyleClass().setAll("lbl-success","lbl");
-		title.getChildren().addAll(titleName,titleVersion);
-		setMargin(title, new Insets(60));
 		
 		start.setOnAction(e->{
 			start.setText("LOADING");
@@ -43,14 +33,18 @@ public class MainPane extends VBox implements IStoppable {
 		});
 		start.getStyleClass().setAll("btn","btn-lg","btn-success");
 		start.setAlignment(Pos.CENTER);
-		setMargin(start, new Insets(10));
+		start.setPrefSize(200, 80);
+		start.setStyle("-fx-cursor: hand;");
+		setMargin(start, new Insets(20,100,20,0));
 		
 		exit.setOnAction(e->{
 			Platform.exit();
 		});
 		exit.getStyleClass().setAll("btn","btn-lg","btn-danger");
 		exit.setAlignment(Pos.CENTER);
-		setMargin(exit, new Insets(10));
+		exit.setPrefSize(200, 80);
+		exit.setStyle("-fx-cursor: hand;");
+		setMargin(exit, new Insets(20,100,20,0));
 		
 		highscore.setOnAction(e->{
 			highscore.setText("Loading");
@@ -63,16 +57,16 @@ public class MainPane extends VBox implements IStoppable {
 						highscore.setText("High score");
 					});
 				} catch (InterruptedException e1) {
-					// TODO Auto-generated catch block
-					
+
 				}
-				
 			});
 			joiner.start();
 		});
 		highscore.getStyleClass().setAll("btn","btn-lg","btn-info");
 		highscore.setAlignment(Pos.CENTER);
-		setMargin(highscore, new Insets(10));
+		highscore.setPrefSize(200, 80);
+		highscore.setStyle("-fx-cursor: hand;");
+		setMargin(highscore, new Insets(20,100,20,0));
 	}
 
 	public void stop() {
